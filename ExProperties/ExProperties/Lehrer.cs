3103a921750ee2c87fb0006cb24a790ab1b55bfb
donduy <do210070@spengergasse.at>
2022-10-07 17:06:01 +0200

    Übung - Properties

---
+++ ExProperties/ExProperties/Lehrer.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Intrinsics.X86;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExProperties {
+    public class Lehrer {
+        private decimal? _bruttogehalt;
+
+        public string Zuname { get; set; } = string.Empty;
+
+        public string Vorname { get; set; } = string.Empty;
+
+        public decimal? Bruttogehalt {
+            get {
+                return _bruttogehalt;
+                }                 
+            
+            set {
+                if(_bruttogehalt == null) {
+                    _bruttogehalt = value;
+                }
+                
+            }
+        }
+
+        public string Kuerzel {            
+            get {
+                if (Zuname.Length >= 3) {                    
+                    Zuname = Zuname.Substring(0,3);
+                    if (Zuname.Length > 0) {
+                        return Zuname.ToUpper();
+                    }                    
+                } 
+                return "";
+            }                
+        }
+        
+        public decimal Nettogehalt {
+            get {
+                return 0.8M*(_bruttogehalt ?? 0);
+            }
+        }
+
+
+    }
+}
---
+++ ExProperties/ExProperties/Rechteck.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExProperties {
+    public class Rechteck {
+        private int _laenge;
+        private int _breite;
+        
+        public int Laenge {
+            get {
+                return _laenge;
+            }
+            set {
+                if(value <0) {
+                    throw new ArgumentException("Ungültige Länge");
+                }
+                _laenge = value;
+            }
+        }
+
+        public int Breite {
+            get {
+                return _breite;
+            }
+            set {
+                if (value < 0) {
+                    throw new ArgumentException("Ungültige Breite");
+                }
+                _breite = value;
+            }
+        }
+        public int Flaeche {
+            get {
+                return _laenge*_breite;
+            }
+        }
+        
+    }
+}
