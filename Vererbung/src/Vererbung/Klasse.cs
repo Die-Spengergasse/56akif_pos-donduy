f4696ef3c9cfc8b8d20d307acdaf65ef3d4106d5
donduy <do210070@spengergasse.at>
2022-11-29 08:22:14 +0100

    Vererbung

---
+++ Vererbung/src/Vererbung/Klasse.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vererbung {
+    public class Klasse {
+        public List<Schueler> ListSchuelers { get; set; } = new();
+        public string Id { get; } = string.Empty;
+        public Lehrer? Kv { get; set; }
+
+        public Klasse(String id) {
+            Id = id;
+        }
+
+        public void Add(Schueler s) {
+            if(s is Schueler) {
+                if ((s is null)) {
+                    throw new ArgumentNullException("Schüler war NULL!");
+                }
+
+                if (ListSchuelers.Contains(s)) {
+                    throw new InvalidOperationException($"Schüler mit der Nr {s.Nr} ist bereits vorhanden!");
+                }
+
+                if(!isIdVorhanden(s)) {
+                    ListSchuelers.Add(s);
+                }                
+
+            } else {
+                throw new InvalidOperationException("Da ist kein Schüler");
+            }
+            
+        }
+
+        public void Remove(Schueler s) {
+            if (s is Schueler) {
+                if ((s is null)) {
+                    throw new ArgumentNullException("Schüler war NULL!");
+                }
+
+                if (ListSchuelers.Contains(s)) {                    
+                    ListSchuelers.Remove(s);
+                } else {
+                    throw new InvalidOperationException("Schüler ist nicht vorhanden.");
+                }                
+            }
+            else {
+                throw new InvalidOperationException("Da ist kein Schüler");
+            }
+        }
+
+        public override string ToString() {
+            //return "Hello World!";
+            return $"Klasse {Id}, KV: {Kv?.Zuname}, Schüleranzahl: {ListSchuelers.Count}";           
+
+        }
+
+        public bool isIdVorhanden(Schueler s) {
+            foreach(Schueler s_iterm in ListSchuelers) {
+                if(s_iterm.Nr == s.Nr) {
+                    s_iterm.Zuname = s.Zuname;
+                    return true;
+                    //break;
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ Vererbung/src/Vererbung/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vererbung {
+    class Program {
+        // Ausgabe:
+        // Ist s1AlsPerson volljährig?          False.
+        // Accountname von s1 ist               eif0001.
+        // Accountname von l1 ist               fleißig.
+        // Accountname von l1AlsPerson ist      fleißig.
+        // ToString() von l1 ist                base.ToString() von Person 1 ist VererbungDemo.Lehrer.
+        // HiddenMethod von s1AlsPerson ist     HiddenMethod in Person.
+
+        static void Main(string[] args) {
+            Klasse k1 = new Klasse("3AHIF") { Kv = new Lehrer(1, "GC") { Zuname = "Gründl" } };
+            Schueler s1 = new Schueler(1) { Zuname = "ZN1" };
+            Schueler s2 = new Schueler(2) { Zuname = "ZN2" };
+            Schueler s3 = new Schueler(1) { Zuname = "ZN3" };
+
+            try {
+                k1.Add(s1);
+                k1.Add(s2);
+                k1.Add(s3);
+            }
+            catch (InvalidOperationException e) {
+                Console.Error.WriteLine(e.Message);
+            }
+            Console.WriteLine(k1);
+            k1.Remove(s1);
+            Console.WriteLine(k1);
+
+        }
+    }
+}
